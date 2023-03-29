c8208e5ca14561584bb033e77868dbb1645a00ec
Nicolas Alan <nicolasalangrotti03@gmail.com>
2023-03-28 21:21:42 -0300

    updade

--- Dockerfile
+++ Dockerfile
@@ -9,7 +9,9 @@ RUN useradd --create-home --home-dir /home/ubuntu --shell /bin/bash --user-group
     echo ubuntu:ubuntu | chpasswd && \
     echo "ubuntu ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
 COPY ./ros-noetic-desktop.sh /ros-noetic-desktop.sh
+
 RUN mkdir -p /tmp/ros_setup_scripts_ubuntu && mv /ros-noetic-desktop.sh /tmp/ros_setup_scripts_ubuntu/ && \
     gosu ubuntu /tmp/ros_setup_scripts_ubuntu/ros-noetic-desktop.sh && \
     rm -rf /var/lib/apt/lists/*
+
 ENV USER ubuntu
--- ros-noetic-desktop.sh
+++ ros-noetic-desktop.sh
@@ -4,6 +4,7 @@ set -eu
 [[ "$(lsb_release -sc)" == "focal" ]] || exit 1
 ROS_DISTRO=noetic
 
+# Install ROS Noetic
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 
 sudo apt-get install -y curl
@@ -20,6 +21,45 @@ rosdep update
 sudo apt-get install -y python3-rosinstall python3-rosinstall-generator python3-wstool build-essential python3-vcstool
 sudo apt-get install -y python3-catkin-tools python3-osrf-pycommon
 
+# Install Vscode
+sudo apt install software-properties-common apt-transport-https wget -y
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt install code
+
+sudo apt-get install -q -y --no-install-recommends 
+sudo apt-get install -q -y --no-install-recommends build-essential 
+sudo apt-get install -q -y --no-install-recommends apt-utils 
+sudo apt-get install -q -y --no-install-recommends cmake 
+sudo apt-get install -q -y --no-install-recommends g++ 
+sudo apt-get install -q -y --no-install-recommends git 
+sudo apt-get install -q -y --no-install-recommends libcanberra-gtk* 
+sudo apt-get install -q -y --no-install-recommends python3-catkin-tools 
+sudo apt-get install -q -y --no-install-recommends python3-pip 
+sudo apt-get install -q -y --no-install-recommends python3-tk 
+sudo apt-get install -q -y --no-install-recommends python3-yaml 
+sudo apt-get install -q -y --no-install-recommends python3-dev 
+sudo apt-get install -q -y --no-install-recommends python3-numpy 
+sudo apt-get install -q -y --no-install-recommends python3-rosinstall 
+sudo apt-get install -q -y --no-install-recommends python3-catkin-pkg 
+sudo apt-get install -q -y --no-install-recommends python3-rosdistro 
+sudo apt-get install -q -y --no-install-recommends python3-rospkg 
+
+# Install dependencies ros
+sudo apt-get install -y ros-noetic-ros-controllers 
+sudo apt-get install -y ros-noetic-joint-state-controller 
+sudo apt-get install -y ros-noetic-joint-state-publisher 
+sudo apt-get install -y ros-noetic-robot-state-publisher 
+sudo apt-get install -y ros-noetic-robot-state-controller 
+sudo apt-get install -y ros-noetic-xacro 
+sudo apt-get install -y ros-noetic-smach-ros 
+sudo apt-get install -y ros-noetic-gazebo-ros 
+sudo apt-get install -y ros-noetic-gazebo-ros-control 
+sudo apt-get install -y ros-noetic-rplidar-ros 
+sudo apt-get install -y ros-noetic-driver-base 
+sudo apt-get install -y ros-noetic-rosserial-arduino
+
+# Setup Env
 grep -F "source /opt/ros/${ROS_DISTRO}/setup.bash" ~/.bashrc ||
 echo "source /opt/ros/${ROS_DISTRO}/setup.bash" >> ~/.bashrc
 
